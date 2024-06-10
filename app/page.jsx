5779185665bef6a809bc101f7157da02260ff125
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-06-10 19:14:33 +1000

    added and sized image

--- app/page.jsx
+++ app/page.jsx
@@ -12,13 +12,13 @@ const Home = () => {
       <div className="container mx-auto h-full">
         <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
           {/* text */}
-          <div className="text-center xl:text-left">
-            <span className="text-xl ">Software Developer</span>
+          <div className="text-center xl:text-left order-2 xl:order-none">
+            <span className="text-xl ">Ubermensch</span>
             <h1 className="h1 mb-6">
               Hello I'm <br /> <span className="text-accent">Stirling Donaldson</span>
             </h1>
             <p className="max-w-[500px] mb-9 text-white/80">
-              I excel at crafting elegant digital experiences and I am proficient in various programming languages and technologies.
+              What do I do? Web-development, SEO, UX-design. Beautifully, competently, and creatively.
             </p>
             {/* button and socials*/}
             <div className="flex flex-col xl:flex-row items-center gap-8">
@@ -36,7 +36,7 @@ const Home = () => {
             </div>
           </div>
           {/* photo */}
-          <div>
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
             <Photo />
           </div>
         </div>
