788042d4146e1f2a8312266f29ccf9450fa3d624
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-06-10 16:44:45 +1000

    mobile and desktop navbars complete

--- components/MobileNav.jsx
+++ components/MobileNav.jsx
@@ -6,6 +6,7 @@ import Link from 'next/link';
 import {CiMenuFries} from 'react-icons/ci';
 
 const links = [
+
     {
         name: 'home',
         path: '/',
@@ -26,7 +27,7 @@ const links = [
         name: 'contact',
         path: '/contact',
     },
-]
+];
 
 const MobileNav = () => {
     const pathname = usePathname();
@@ -48,14 +49,7 @@ const MobileNav = () => {
                 <nav className='flex flex-col justify-center items-center gap-8'>
                     {links.map((link, index) => {
                         return (
-                            <Link 
-                                href={link.path},
-                                key={index} 
-                                className={'${
-                                    link.path === pathname && 
-                                    "text-accent border-b-2 border-accent"
-                                }   text-xl capitalize hover:text-accent transition-all'}
-                            >
+                            <Link href={link.path} key={index} className={`${link.path === pathname ? "text-accent border-b-2 border-accent" : ""} text-xl capitalize hover:text-accent transition-all`}>
                                 {link.name}
                             </Link>
                         );
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,7 +25,7 @@ const links = [
         path: "/contact",
     },
 
-]
+];
 
 const Nav = () => {
     const pathname = usePathname();
@@ -33,17 +33,13 @@ const Nav = () => {
         <nav className="flex gap-8">
         {links.map((link, index) => {
             return (
-                <Link 
-                    href={link.path} 
-                    key={index} 
-                    className={'${link.path === pathname && "text-accent border-b-2 border-accent"} capitalize font-medium hover:text-accent transition-all'}>
+                <Link href={link.path} key={index} className={`${link.path === pathname ? "text-accent border-b-2 border-accent" : ""} text-xl capitalize hover:text-accent transition-all`}>
                     {link.name}
                 </Link>
             );
         })}
         </nav>
     );
-  
-}
+};
 
 export default Nav
