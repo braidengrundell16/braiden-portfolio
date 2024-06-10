4f138f6c6c5c76f2713b4e2418677bc17c03b6a0
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-06-10 18:37:14 +1000

    added image, and formatted it

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,9 +1,30 @@
-
-
+"use client";
+import { motion } from "framer-motion";
+import Image from "next/image";
 
 const Photo = () => {
   return (
-    <div>Photo</div>
+    <div className="w-full h-full relative">
+        <motion.div 
+            initial={{opacity: 0}} 
+            animate={{
+                opacity: 1, 
+                transition: {delay: 2, duration: 0.4, ease:"easeIn"},
+            }}>
+            <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+                <Image
+                    src="/assets/photo.png" 
+                    priority 
+                    quality={100} 
+                    alt="" 
+                    height={500}
+                    width={500}
+                    
+                    className="rounded-full"
+                />
+            </div>
+        </motion.div>
+    </div>
   )
 }
 
