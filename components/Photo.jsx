783ca1dce69193df32e53b637e6b8cb8eeea5c56
stirlingdonaldson <stirlingwdonaldson@gmail.com>
2024-06-10 18:42:54 +1000

    changed <Image ?> options, so my face is smaller

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -10,19 +10,25 @@ const Photo = () => {
             animate={{
                 opacity: 1, 
                 transition: {delay: 2, duration: 0.4, ease:"easeIn"},
-            }}>
-            <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten">
+            }}
+        >
+            <motion.div 
+                initial={{opacity: 0}} 
+                animate={{
+                    opacity: 1, 
+                    transition: {delay: 2.4, duration: 0.4, ease:"easeInOut"},
+                }}
+                className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten"
+            >
                 <Image
                     src="/assets/photo.png" 
                     priority 
                     quality={100} 
+                    fill
                     alt="" 
-                    height={500}
-                    width={500}
-                    
-                    className="rounded-full"
+                    className="rounded-full object-contain"
                 />
-            </div>
+            </motion.div>
         </motion.div>
     </div>
   )
